420ca38990346928e9a15ddc633c047625bfa217
Connor Dailey <connorldailey@gmail.com>
2024-12-18 12:04:00 -0500

    Add User model

---
+++ src/models/User.ts
@@ -0,0 +1,62 @@
+import { Schema, model, type Document } from 'mongoose';
+
+interface IUser extends Document {
+    username: string,
+    email: string,
+    thoughts: Schema.Types.ObjectId[],
+    friends: Schema.Types.ObjectId[]
+}
+
+const validateEmail = function(email: string) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+};
+
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: validateEmail,
+                message: 'Please provide a valid email address',
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        timestamps: true
+    },
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function (this: IUser) {
+        return this.friends.length;
+    });
+
+const User = model('User', userSchema);
+
+export default User;
